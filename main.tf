e228f7a90afd4e4058e36ad9f58ffd13fb5a1681
Nikhil Thomas <nikhilthomas1@gmail.com>
2025-01-24 16:01:00 -0500

    vault config with 2 vaults
    Signed-off-by: Nikhil Thomas <nikhilthomas1@gmail.com>

--- main.tf
+++ main.tf
@@ -1,14 +1,27 @@
 # providers.tf contents
 provider "vault" {
-  address = "http://127.0.0.1:8200"  # Change based on your Vault instance
-  token   = "myroot"  # Use your Vault root token
+  alias   = "dev"
+  address = "http://127.0.0.1:8200"  # Address for the first Vault instance
+  token   = "myroot"  # Use your Vault root token for the first instance
+}
+
+provider "vault" {
+  alias   = "prod"
+  address = "http://127.0.0.1:8201"  # Address for the second Vault instance
+  token   = "myroot"  # Use your Vault root token for the second instance
 }
 
 provider "local" {}
 
 # data_sources.tf contents
 data "vault_generic_secret" "dev" {
-  path = "secret/myapp/config"
+  provider = vault.dev
+  path     = "secret/myapp/config"
+}
+
+data "vault_generic_secret" "prod" {
+  provider = vault.prod
+  path     = "secret/myapp/config"
 }
 
 resource "local_file" "username_file" {
@@ -16,6 +29,15 @@ resource "local_file" "username_file" {
   filename = "${path.module}/username.txt"
 }
 
+resource "local_file" "prod_username_file" {
+  content  = data.vault_generic_secret.prod.data["username"]
+  filename = "${path.module}/prod_username.txt"
+}
+
 output "username_file_path" {
   value = local_file.username_file.filename
+}
+
+output "prod_username_file_path" {
+  value = local_file.prod_username_file.filename
 }
